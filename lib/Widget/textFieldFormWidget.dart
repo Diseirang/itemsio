c6c4a7faf570156a40a9dbece7a9b22b7bdabba7
DiseirangCHHEUN <111745190+Diseirang@users.noreply.github.com>
2022-11-27 15:47:37 +0700

    Saved

--- lib/Widget/textFieldFormWidget.dart
+++ lib/Widget/textFieldFormWidget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'customDatePicker.dart';
 import 'textField.dart';
 
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
+      //debugShowCheckedModeBanner: false,
+      ////////////////////
       home: SplashScreen(),
     );
   }
